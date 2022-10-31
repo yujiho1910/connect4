2c6d5ace4e762fc3682ede47a190e24a69a8b038
Ho Jing Rui <64954746+yujiho1910@users.noreply.github.com>
2022-11-01 01:12:38 +0800

    removed the rows code

--- connect4.py
+++ connect4.py
@@ -10,10 +10,6 @@ Choose your options:
 3) Player vs Player
 """
 
-rows_message = """
-Enter no. of rows (minimum 4):
-"""
-
 def intInput(a, b, msg):
     while True:
         try:
@@ -64,9 +60,6 @@ def menu():
     # implement your function here
     print("Welcome!")
     choice = intInput(1,3, choices_message)
-    rows = intInput(4,float('inf'),rows_message)
-    board = [0] * 7 * rows
-    display_board(board)
     pass
 
 if __name__ == "__main__":
