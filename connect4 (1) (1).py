8adf31440018c8b5b476bcb9372c774214c1766f
yujiho1910 <yujiho54@gmail.com>
2022-10-31 20:20:59 +0800

    adding skeleton files

---
+++ connect4 (1) (1).py
@@ -0,0 +1,34 @@
+import random
+import math
+
+def check_move(board, turn, col, pop):
+    # implement your function here
+    return True
+
+def apply_move(board, turn, col, pop):
+    # implement your function here
+    return board.copy()
+
+def check_victory(board, who_played):
+    # implement your function here
+    return -1
+
+def computer_move(board, turn, level):
+    # implement your function here
+    return (0,False)
+    
+def display_board(board):
+    # implement your function here
+    pass
+
+def menu():
+    # implement your function here
+    pass
+
+if __name__ == "__main__":
+    menu()
+
+
+
+
+    
